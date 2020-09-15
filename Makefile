c706b3e0a7531ca3f748c31997752377ccd79108
Chao <robertfrost0924@gmail.com>
2020-09-15 02:57:23 +0000

    update

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+setup:
+	python3 -m venv virt
+
 install:
 	pip install --upgrade pip &&\
-	pip install boto3
+	pip install boto3 &&\
+	pip freeze > requirements.txt &&\
+	pip install -r requirements.txt
+
+
+	
--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import boto3
 comprehend = boto3.client("comprehend")
 
 text = input("Please enter a message:")
+language = input("Please enter your language:")
 
-print(comprehend.detect_key_phrases(Text = text, LanguageCode = 'en'))
+print(comprehend.detect_key_phrases(Text = text, LanguageCode = language))
