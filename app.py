139717ffbe68150e9b06d963a99c619430d958d9
oscar <oscar@MacBook-Pro-2.local>
2020-09-16 12:38:29 +0800

    flask demo

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import boto3
+from flask import Flask, render_template
+#
+# comprehend = boto3.client("comprehend")
+#
+# text = input("Please enter a message:")
+# language = input("Please enter your language:")
+#
+# print(comprehend.detect_key_phrases(Text = text, LanguageCode = language))
 
-comprehend = boto3.client("comprehend")
+app = Flask(__name__)
 
-text = input("Please enter a message:")
-language = input("Please enter your language:")
-
-print(comprehend.detect_key_phrases(Text = text, LanguageCode = language))
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == "main":
+    app.run(debug=True)
