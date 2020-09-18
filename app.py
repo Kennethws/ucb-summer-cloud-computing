63017ccab2bd323a1e826b38d6f26cf2364548aa
Chao <robertfrost0924@gmail.com>
2020-09-18 07:26:25 +0000

    update app

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def text_detect():
 		return render_template("boto.html", result=result )
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True, host='0.0.0.0', port=8080)
