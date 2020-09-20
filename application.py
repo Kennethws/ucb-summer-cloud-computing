d85000c199479a658e521992b72c240378ffea30
Chao <robertfrost0924@gmail.com>
2020-09-20 13:43:06 +0000

    update app

--- application.py
+++ application.py
@@ -5,17 +5,17 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @application.route('/boto', methods = ['POST', 'GET'])
 def text_detect():
-	
-	if request.method == 'POST':
-		comprehend = boto3.client("comprehend", region_name='us-west-2')
-		text = request.form.get('text')
-		language = request.form.get('language')
-		result = comprehend.detect_key_phrases(Text = text, LanguageCode = language)
-		return render_template("boto.html", result=result )
+    if request.method == 'POST':
+        comprehend = boto3.client("comprehend", region_name='us-west-2')
+        text = request.form.get('text')
+        language = request.form.get('language')
+        result = comprehend.detect_key_phrases(Text = text, LanguageCode = language)
+        return render_template("boto.html", result=result )
 
 if __name__ == '__main__':
-   application.run(host='0.0.0.0', port=8080, debug=True)
+    # pylint: disable=no-value-for-parameter
+    application.run(host='0.0.0.0', port=8080, debug=True)
