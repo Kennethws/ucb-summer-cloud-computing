261fd9e88ad434e64afd69b462140bb7244304b1
Chao <robertfrost0924@gmail.com>
2020-09-21 04:38:28 +0000

    for CodeBuild

--- Makefile
+++ Makefile
@@ -3,9 +3,6 @@ setup:
 
 install:
 	pip install --upgrade pip &&\
-	pip install boto3 &&\
-	pip install Flask &&\
-	pip3 install awsebcli &&\
 	pip install -r requirements.txt &&\
 	pip freeze > requirements.txt
 
--- test_application.py
+++ test_application.py
@@ -2,17 +2,19 @@
 from flask import Flask, render_template
 import boto3
 
-test_application = Flask(__name__)
+def test():
 
-@test_application.route('/')
-def text_detect():
-    comprehend = boto3.client('comprehend', region_name='us-west-2')
-    text = 'Kobe Bean Bryant (/ˈkoʊbiː/ KOH-bee; August 23, 1978 – January 26, 2020) was an American professional basketball player. A shooting guard, he spent his entire career with the Los Angeles Lakers in the National Basketball Association (NBA).'
-    language = 'en'
-    result = comprehend.detect_key_phrases(Text = text, LanguageCode = language)
-    return render_template('boto.html', result = result)
+    test_application = Flask(__name__)
 
-if __name__ == '__main__':
-    test_application.run(host='0.0.0.0', port=8080, debug=True)
+    @test_application.route('/')
+    def text_detect():
+        comprehend = boto3.client('comprehend', region_name='us-west-2')
+        text = 'Kobe Bean Bryant (/ˈkoʊbiː/ KOH-bee; August 23, 1978 – January 26, 2020) was an American professional basketball player. A shooting guard, he spent his entire career with the Los Angeles Lakers in the National Basketball Association (NBA).'
+        language = 'en'
+        result = comprehend.detect_key_phrases(Text = text, LanguageCode = language)
+        return render_template('boto.html', result = result)
+    
+    if __name__ == '__main__':
+        test_application.run(host='0.0.0.0', port=8080, debug=True)
     
     
