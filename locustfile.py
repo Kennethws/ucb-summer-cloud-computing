98a0856a47251968db168a70b5083115f4087da7
Chao Wang <robertfrost0924@gmail.com>
2020-09-27 13:22:44 +0000

    update locustfile

--- locustfile.py
+++ locustfile.py
@@ -4,13 +4,12 @@ class UserBehavior(SequentialTaskSet):
 
 	@task
 	def submit(self):
-			self.client.post('/', {'text': 'Kobe Bryant is the best NBA player.', 'language': 'en'})
+		self.client.post('/', {'text': 'Kobe Bryant is the best NBA player.', 'language': 'en'})
 	
 	@task	
 	def get_boto(self):
-			self.client.get('/boto')
+		self.client.get('/boto')
 
 class WebsiteUser(HttpUser):
 	tasks = [UserBehavior]
-	host = "http://boto3-env.eba-8zj3bgu3.us-west-2.elasticbeanstalk.com/"
 	wait_time = between(1, 2)
