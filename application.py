b94a4c46b7466db17fbb21a7b4aae4032dc22c38
Xiangyang Han <xiangyanghan96@gmail.com>
2020-09-30 03:26:59 -0400

    Han: add view and functions for showing generated handwritten picture

--- application.py
+++ application.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
+import json, os
+import cv2
 import boto3
+from base64 import b64decode
+import svglib
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
 application = Flask(__name__)
-
 
 
 @application.route('/')
@@ -20,8 +25,37 @@ def text_detect():
 
 @application.route('/signature')
 def jsignature():
-
 	return render_template('unmini.html')
+
+#---------------Callback function for Ajax from JQuery---------------------------
+@application.route('/recognize', methods = ['POST', 'GET'])
+def recognize():
+	#---------get img data(svg/base64) from jquery and generate .jpg image.------
+	data_uri = request.form.get('data')
+	header, encoded = data_uri.split(",", 1)
+	data = b64decode(encoded)
+	with open("handwritten.svg", "wb") as f: 
+		f.write(data)
+	drawing = svg2rlg("handwritten.svg")
+	renderPM.drawToFile(drawing, "handwritten.jpg", fmt="JPG")
+	if os.path.exists("handwritten.svg"):
+		os.remove("handwritten.svg")
+	else:
+		print("The file does not exist")
+
+	src = cv2.imread("handwritten.jpg", cv2.IMREAD_GRAYSCALE)
+	resize_image = cv2.resize(src, (256, 64), interpolation=cv2.INTER_LINEAR)
+	cv2.imwrite("handwritten.jpg", resize_image)
+
+	#-------Comment below if want to see the exact picture---------
+	if os.path.exists("handwritten.jpg"):
+		os.remove("handwritten.jpg")
+	else:
+		print("The file does not exist")
+	#---------------------------------------------------------------
+	
+	return data
+
 
 if __name__ == '__main__':
    application.run(debug=True)
